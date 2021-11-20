9f6d4b0e6fa9aad3a4ab3beb962290ba8a578dd2
paulhemingway <phemingway@ymail.com>
2021-11-20 13:55:20 -0600

    added a navbar

--- code/hackweek/src/app/app.module.ts
+++ code/hackweek/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild([
+      { path: '', component: HomeComponent},
+      { path: 'home', component: HomeComponent},
+      { path: 'profile', component: ProfileComponent},
+      { path: 'post', component: CreatePostComponent},
+      { path: '**', component: HomeComponent}
+    ]),
+    NgbModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
