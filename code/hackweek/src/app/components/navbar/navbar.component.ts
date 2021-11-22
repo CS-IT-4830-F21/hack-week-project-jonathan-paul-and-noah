099b3eb3d878597c487484651a8006171b8ce555
paulhemingway <phemingway@ymail.com>
2021-11-22 14:09:28 -0600

    create account page component

--- code/hackweek/src/app/components/navbar/navbar.component.ts
+++ code/hackweek/src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  loggedIn = true;
+  loggedIn = false;
   constructor() { }
 
   ngOnInit(): void {
