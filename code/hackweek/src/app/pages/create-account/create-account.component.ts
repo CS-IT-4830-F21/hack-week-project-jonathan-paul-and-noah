b2af8053d9aba26da2779c55e2767182a51ae1b3
paulhemingway <phemingway@ymail.com>
2021-11-22 15:38:37 -0600

    start creating the create account page

--- code/hackweek/src/app/pages/create-account/create-account.component.ts
+++ code/hackweek/src/app/pages/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() { 
+    
+  }
+  @ViewChild('profilePic') myInputVariable!: ElementRef;
 
   ngOnInit(): void {
+  }
+
+  url: any; 
+	msg = "";
+	
+
+	selectFile(event: any) { 
+		if(!event.target.files[0] || event.target.files[0].length == 0) {
+			this.msg = 'You must select an image';
+			return;
+		}
+		
+		var mimeType = event.target.files[0].type;
+		
+		if (mimeType.match(/image\/*/) == null) {
+			this.msg = "Only images are supported";
+			return;
+		}
+		
+		var reader = new FileReader();
+		reader.readAsDataURL(event.target.files[0]);
+		
+		reader.onload = (_event) => {
+			this.msg = "";
+			this.url = reader.result; 
+		}
+    console.log(this.url)
+	}
+  
+  clearFile(){
+    this.myInputVariable.nativeElement.value = "";
+    this.url = ""
   }
 
 }
