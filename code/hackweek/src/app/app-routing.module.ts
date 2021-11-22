b02e99ca7b1edc257214337ebaedf027155c7d82
swimmingnoah <54280709+swimmingnoah@users.noreply.github.com>
2021-11-22 12:49:35 -0600

    added routing to app routing module

--- code/hackweek/src/app/app-routing.module.ts
+++ code/hackweek/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'post', component: CreatePostComponent},
+  { path: '**', component: HomeComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
